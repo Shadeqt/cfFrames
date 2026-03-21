3c0688e9c096cd81b82ab360be206c22f39f1cd6
Shadeqt <itkofod@gmail.com>
2026-03-21 16:23:09 +0100

    Fix nameplate castbar taint and flash issues
    Create castbar on UIParent to avoid restricted region taint during OnLoad, then reparent to nameplate UnitFrame.

--- NameplateCastbar.lua
+++ NameplateCastbar.lua
@@ -3,25 +3,36 @@ local M = cfFrames.MODULES
 local function GetCastBar(nameplate, unit)
 	if nameplate.cfCastBar then return nameplate.cfCastBar end
 
-	local bar = CreateFrame("StatusBar", nil, nameplate, "SmallCastingBarFrameTemplate")
-	local healthBar = nameplate.UnitFrame.healthBar
+	local unitFrame = nameplate.UnitFrame
+	local bar = CreateFrame("StatusBar", nil, UIParent, "SmallCastingBarFrameTemplate")
+	bar:Hide()
+
+	-- Clear template points before OnLoad to avoid taint from GetPoint() on restricted regions
+	bar.Border:ClearAllPoints()
+	bar.Spark:ClearAllPoints()
+	bar.Icon:ClearAllPoints()
+	bar.Flash:ClearAllPoints()
+	bar.Text:ClearAllPoints()
+	if bar.BorderShield then bar.BorderShield:ClearAllPoints() end
+
+	CastingBarFrame_OnLoad(bar, unit)
+
+	-- Reparent to nameplate after OnLoad
+	bar:SetParent(unitFrame)
+
+	local healthBar = unitFrame.healthBar
 	bar:SetPoint("TOP", healthBar, "BOTTOM", 0, -5)
 	bar:SetSize(healthBar:GetWidth(), healthBar:GetHeight())
 
-	bar.Flash:ClearAllPoints()
-	bar.Flash:SetPoint("TOPLEFT", -18, 16)
-	bar.Flash:SetPoint("BOTTOMRIGHT", 16.5, -17)
+	hooksecurefunc(bar.Flash, "Show", function(self) self:Hide() end)
 
-	bar.Border:ClearAllPoints()
 	bar.Border:SetPoint("TOPLEFT", -18, 16)
 	bar.Border:SetPoint("BOTTOMRIGHT", 16.5, -17)
 
-	bar.Icon:ClearAllPoints()
+	bar.Spark:SetSize(16, 16)
+
 	bar.Icon:SetPoint("LEFT", bar, "RIGHT", 8, 0)
 	bar.Icon:SetSize(14, 14)
-
-	CastingBarFrame_OnLoad(bar, unit)
-	CastingBarFrame_SetUnit(bar, unit)
 
 	nameplate.cfCastBar = bar
 	return bar
@@ -32,11 +43,13 @@ frame:RegisterEvent("NAME_PLATE_UNIT_ADDED")
 frame:RegisterEvent("NAME_PLATE_UNIT_REMOVED")
 frame:SetScript("OnEvent", function(_, event, unit)
 	if not cfFramesDB[M.NAMEPLATE_CASTBAR] then return end
-	local nameplate = C_NamePlate.GetNamePlateForUnit(unit)
+	local nameplate = C_NamePlate.GetNamePlateForUnit(unit, issecure())
 	if not nameplate then return end
 
 	if event == "NAME_PLATE_UNIT_ADDED" then
-		GetCastBar(nameplate, unit)
+		local bar = GetCastBar(nameplate, unit)
+		bar.unit = unit
+		CastingBarFrame_SetUnit(bar, unit)
 	elseif event == "NAME_PLATE_UNIT_REMOVED" then
 		if nameplate.cfCastBar then
 			CastingBarFrame_SetUnit(nameplate.cfCastBar, nil)
